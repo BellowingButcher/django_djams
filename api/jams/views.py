d65173bde76e3be9e27c418593cafd00bfc2af9d
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-16 15:52:56 +0000

    Created post operation for song
    - validator, saver, informer, built up the response to inform the front end

--- api/jams/views.py
+++ api/jams/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Songs
 from .serializers import SongsSerializer
-# Create your views here.
+# Create views here.
 
 # https://www.youtube.com/watch?v=x-0xWAJR3Fw This is the first video I watched to start building my viewset
 class SongsAPIView(APIView):
@@ -26,3 +26,22 @@ class SongsAPIView(APIView):
             return Response(serializer.data)
 
     # create operation
+    def post(self, request, format=None):
+        data = request.data
+        serializer = SongsSerializer(data=data)
+
+        # check if data is valid
+        serializer.is_valid(raise_exception=True)
+
+        # Save the Song
+        serializer.save()
+
+        # Inform front end of result
+        response = Response()
+
+        response.data = {
+            'message': 'Song Created Successfully',
+            'data': serializer.data,
+        }
+
+        return response
