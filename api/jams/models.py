5b825336b06b9e3a0ec93913bbaef76d3dd0c46e
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-16 14:48:46 +0000

    Have added songs and artists in models

--- api/jams/models.py
+++ api/jams/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Songs(models.Model):
+    title = models.CharField(max_length=50)
+    duration = models.FloatField(default=0)
+    plays = models.BigIntegerField(default=0)
+
+class Artists(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField(default='Bio goes here')
+    img_url = models.URLField(max_length=200, default=None)
