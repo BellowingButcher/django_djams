102c6dc45cd102f42109ffb33cffba31cfa645b9
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-16 15:20:26 +0000

    added serializers for each model I have

---
+++ api/jams/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Songs, Artists, Genres, Albums, Playlists, Managers
+
+class SongsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Songs
+        fields = '__all__'
+
+class ArtistsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = '__all__'
+
+class GenresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genres
+        fields = '__all__'
+
+class AlbumsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Albums
+        fields = '__all__'
+
+class PlaylistsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Playlists
+        fields = '__all__'
+
+class ManagersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Managers
+        fields = '__all__'
