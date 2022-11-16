d3843a778be37d8411f0e410781f063f36a2c35b
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-16 15:08:39 +0000

    all tables without relations built

--- api/jams/models.py
+++ api/jams/models.py
@@ -6,6 +6,19 @@ class Songs(models.Model):
     plays = models.BigIntegerField(default=0)
 
 class Artists(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     bio = models.TextField(default='Bio goes here')
     img_url = models.URLField(max_length=200, default=None)
+
+class Genres(models.Model):
+    keyword = models.CharField(max_length=20, unique=True)
+
+class Albums(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+class Playlists(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+class Managers(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
