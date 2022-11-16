9c35f1f1fcdff1ae0864d8d01366fcfac76a3f77
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-16 20:41:08 +0000

    added my foreign keys to songs and artists

--- api/jams/models.py
+++ api/jams/models.py
@@ -5,12 +5,15 @@ class Songs(models.Model):
     duration = models.FloatField(default=0)
     plays = models.BigIntegerField(default=0)
     is_explicit = models.BooleanField(default=False)
+    album_id = models.ForeignKey('Albums', default=None, on_delete=models.CASCADE)
+    artist_id = models.ForeignKey('Artists', default=None, on_delete=models.CASCADE)
+    playlist_id = models.ForeignKey('Playlists', default=None, on_delete=models.CASCADE)
 
 class Artists(models.Model):
     name = models.CharField(max_length=50, unique=True)
     bio = models.TextField(default='Bio goes here')
     img_url = models.URLField(max_length=200, default=None)
-
+    manager_id = models.ForeignKey('Managers', default=None, on_delete=models.CASCADE)
 class Genres(models.Model):
     keyword = models.CharField(max_length=20, unique=True)
 
