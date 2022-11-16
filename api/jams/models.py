729e22d29226fe124ca6efa84266a098d57beec5
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-11-16 21:12:01 +0000

    added many to many model albumsong

--- api/jams/models.py
+++ api/jams/models.py
@@ -5,15 +5,14 @@ class Songs(models.Model):
     duration = models.FloatField(default=0)
     plays = models.BigIntegerField(default=0)
     is_explicit = models.BooleanField(default=False)
-    album_id = models.ForeignKey('Albums', default=None, on_delete=models.CASCADE)
-    artist_id = models.ForeignKey('Artists', default=None, on_delete=models.CASCADE)
-    playlist_id = models.ForeignKey('Playlists', default=None, on_delete=models.CASCADE)
+    # artist_id = models.ForeignKey('Artists', default=None, on_delete=models.CASCADE)
+    # playlist_id = models.ForeignKey('Playlists', default=None, on_delete=models.CASCADE)
 
 class Artists(models.Model):
     name = models.CharField(max_length=50, unique=True)
     bio = models.TextField(default='Bio goes here')
     img_url = models.URLField(max_length=200, default=None)
-    manager_id = models.ForeignKey('Managers', default=None, on_delete=models.CASCADE)
+    # manager_id = models.ForeignKey('Managers', default=None, on_delete=models.CASCADE)
 class Genres(models.Model):
     keyword = models.CharField(max_length=20, unique=True)
 
@@ -26,3 +25,8 @@ class Playlists(models.Model):
 class Managers(models.Model):
     name = models.CharField(max_length=25, unique=True)
     email = models.EmailField(max_length=254, unique=True)
+
+class AlbumsSongs(models.Model):
+    song_id = models.ForeignKey(Songs, default=None, on_delete=models.CASCADE)
+    album_id = models.ForeignKey(Albums, default=None, on_delete=models.CASCADE)
+
